32d4d2119fcbcbdb55c73e31ea0f9fc044b12867
unknown <miguel.perez99@hotmail.com>
2021-03-26 12:46:10 -0500

    listo para que el julian trabje en el fron ahora si

--- src/app/AsignacionDocente/add-asig/add-asig.component.ts
+++ src/app/AsignacionDocente/add-asig/add-asig.component.ts
@@ -17,65 +17,93 @@ import { MateriaService } from 'src/app/Service/materia.service';
 })
 export class AddAsigComponent implements OnInit {
 
-  asignacionDocente: AsignacionDocente = new AsignacionDocente;
+  asignacionDocente: AsignacionDocente = new AsignacionDocente();
+  asignacionDocentes: AsignacionDocente[] = []
   hide: boolean = true;
   message: string = "";
-  listaDocentes: Docente[]=[];
-  listaCursos: Curso[]=[];
-  listaMaterias: Materia[]=[];
+  listaDocentes: Docente[] = [];
+  listaCursos: Curso[] = [];
+  listaMaterias: Materia[] = [];
+  listaMaterias2: Materia[] = [];
+  listaMaterias3: Materia[] = [];
 
-  constructor(private router:Router, private service:AsignacionDocenteService, 
-    private cursoService: CursoService, private docenteService:DocenteService,
+  constructor(private router: Router, private service: AsignacionDocenteService,
+    private cursoService: CursoService, private docenteService: DocenteService,
     private materiaService: MateriaService) { }
 
   ngOnInit(): void {
-    this.docenteService.listar().subscribe(res=>{this.listaDocentes=res});
-    this.cursoService.listar().subscribe(res=>{this.listaCursos=res});
-    this.materiaService.listar().subscribe(res=>{this.listaMaterias=res});
+    this.docenteService.listar().subscribe(res => { this.listaDocentes = res });
+    this.cursoService.listar().subscribe(res => { this.listaCursos = res });
+    this.materiaService.listar().subscribe(res => { this.listaMaterias = res });
+    this.service.listar().subscribe(data => { this.asignacionDocentes = data; });
   }
 
-  getNombreDocente() {    
+  obtenerListaMateriasPorCurso(event: any): void {
+    this.listaMaterias2 = [];
+    this.listaMaterias3 = [];
+    //guarda en listaMaterias2 los materias por docente ya asignadas
+    this.asignacionDocentes.forEach(asignacionDocente => {
+      if (this.asignacionDocente.curso.idCurso == asignacionDocente.curso.idCurso && asignacionDocente.materia.estado == true) {
+        this.listaMaterias2.push(asignacionDocente.materia);
+      }
+    });
+    //compara si la listaMaterias y listaMaterias2 son iguales; si no lo son guarda esa matteria en la listaMaterias3   
+    var flag:boolean = true;
+    this.listaMaterias.forEach(Materia1 => {
+      flag = true;
+      this.listaMaterias2.forEach(Materia2 => {
+        if (Materia1.idMateria == Materia2.idMateria) {
+            flag=false;
+        }
+      });
+      if(flag){
+        this.listaMaterias3.push(Materia1);
+      }      
+    });
+
+  }
+
+  getNombreDocente() {
     this.listaDocentes.forEach(Docente => {
-      if (Docente.idDocente==this.asignacionDocente.docente.idDocente){                
-        this.asignacionDocente.docente = Docente;        
+      if (Docente.idDocente == this.asignacionDocente.docente.idDocente) {
+        this.asignacionDocente.docente = Docente;
       }
-    });    
+    });
   }
 
-  getNombreCurso() {    
+  getNombreCurso() {
     this.listaCursos.forEach(Curso => {
-      if (Curso.idCurso==this.asignacionDocente.curso.idCurso){                
-        this.asignacionDocente.curso = Curso;        
+      if (Curso.idCurso == this.asignacionDocente.curso.idCurso) {
+        this.asignacionDocente.curso = Curso;
       }
-    });    
+    });
   }
 
-  getNombreMateria() {    
+  getNombreMateria() {
     this.listaMaterias.forEach(Materia => {
-      if (Materia.idMateria==this.asignacionDocente.materia.idMateria){                
-        this.asignacionDocente.materia = Materia;        
+      if (Materia.idMateria == this.asignacionDocente.materia.idMateria) {
+        this.asignacionDocente.materia = Materia;
       }
-    });    
+    });
   }
 
-  Registrar(){
+  Registrar() {
     this.getNombreDocente();
-    console.log(this.asignacionDocente.docente) ;
-    this.getNombreCurso();  
-    this.getNombreMateria();      
-    this.service.save(this.asignacionDocente).subscribe(resultado=>{
+    this.getNombreCurso();
+    this.getNombreMateria();
+    this.service.save(this.asignacionDocente).subscribe(resultado => {
       console.log(resultado);
-          
+
       //alert(resultado[0]);
       var nameError = document.getElementById("nombreError");
-      if(nameError != undefined){
+      if (nameError != undefined) {
         nameError.innerHTML = resultado[0];
       }
-      if(resultado[0]=="Se ha asignado correctamente el curso y la materia"){
-        this.hide=false;
+      if (resultado[0] == "Se ha asignado correctamente el curso y la materia") {
+        this.hide = false;
         this.message = resultado[0];
         document.getElementById("btnRegistrar")?.setAttribute("disabled", "true");
-        setTimeout(()=>{          
+        setTimeout(() => {
           this.router.navigate(["/listarAsignacionDocente"]);
         }, 2000);
       }
